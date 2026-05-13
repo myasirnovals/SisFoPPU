1e32827db40119b56aa4a2560086aa42973dbb24
MYN2997 <hawkinsdamien007@gmail.com>
2026-05-13 09:46:22 +0700

    menambahkan route

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -8,3 +8,4 @@ use CodeIgniter\Router\RouteCollection;
 $routes->get('/', 'Home::index');
 $routes->get('/admin/dashboard', 'Admin\Dashboard::index');
 $routes->get('/admin/matakuliah', 'Admin\MataKuliah::index');
+$routes->get('/admin/template', 'Admin\Template::index');
