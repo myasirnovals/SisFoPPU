e2949184bc9f0784454ff50a61507655ba250368
MYN2997 <hawkinsdamien007@gmail.com>
2026-05-13 07:29:30 +0700

    menambahkan ikon unjani

--- app/Views/layout/admin_layout.php
+++ app/Views/layout/admin_layout.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" href="<?= base_url('assets/images/unjani.png') ?>">
     <title><?= $title ?? 'Admin Panel' ?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
@@ -12,6 +13,7 @@
 
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-4">
         <div class="container">
+            <img src="<?= base_url('assets/images/unjani.png') ?>" alt="Logo Sisfo" width="30" height="30" class="d-inline-block align-text-top me-2">
             <a class="navbar-brand" href="#">Sisfo Praktikum</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
                 <span class="navbar-toggler-icon"></span>
