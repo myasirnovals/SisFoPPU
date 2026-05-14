cd15b91c98f4f38a1e6433d3b20aa64e7642bd32
Bagus DPS <bagusdps631@gmail.com>
2026-05-14 21:20:10 +0700

    Modifikasi File Config/Routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,34 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+
+$routes->get('/', function () {
+    return redirect()->to('/login');
+});
+
+$routes->get('/login', 'AuthController::login');
+$routes->post('/login', 'AuthController::attemptLogin');
+$routes->get('/logout', 'AuthController::logout');
+
+$routes->group('', ['filter' => 'auth'], function ($routes) {
+    $routes->get('/dashboard', 'DashboardController::index');
+});
+
+/*
+ * Contoh route berdasarkan role
+ */
+$routes->group('admin', ['filter' => 'role:admin'], function ($routes) {
+    $routes->get('dashboard', 'DashboardController::index');
+});
+
+$routes->group('dosen', ['filter' => 'role:dosen,koordinator,admin'], function ($routes) {
+    $routes->get('dashboard', 'DashboardController::index');
+});
+
+$routes->group('asisten', ['filter' => 'role:asisten,dosen,koordinator,admin'], function ($routes) {
+    $routes->get('dashboard', 'DashboardController::index');
+});
+
+$routes->group('mahasiswa', ['filter' => 'role:mahasiswa'], function ($routes) {
+    $routes->get('dashboard', 'DashboardController::index');
+});
