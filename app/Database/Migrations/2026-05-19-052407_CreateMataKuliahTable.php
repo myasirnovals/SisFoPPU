c50813d7e24cf1aca2e4f4b1b45259b7fbef0003
MYN2997 <hawkinsdamien007@gmail.com>
2026-05-19 12:37:21 +0700

    menambahkan db admin

---
+++ app/Database/Migrations/2026-05-19-052407_CreateMataKuliahTable.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateMataKuliahTable extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'kode_mk' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '20',
+                'unique'     => true,
+            ],
+            'nama_mk' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '100',
+            ],
+            'sks' => [
+                'type'       => 'INT',
+                'constraint' => 2,
+            ],
+            'prodi_id' => [
+                'type'       => 'INT',
+                'constraint' => 11,
+                'unsigned'   => true,
+                'null'       => true,
+            ],
+            'created_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+            'updated_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+        ]);
+
+        $this->forge->addKey('id', true);
+        $this->forge->addForeignKey('prodi_id', 'prodi', 'id', 'CASCADE', 'SET NULL');
+        $this->forge->createTable('mata_kuliah');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('mata_kuliah');
+    }
+}
